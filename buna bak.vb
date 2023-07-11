28662bcbbce517e11a5d7350da64bbb71da53e94
fatma <123814832+fatmaakpunar@users.noreply.github.com>
2023-07-11 12:24:34 +0300

    Add files via upload

---
+++ buna bak.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Dim A, B, C As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        A = 12
+        B = 7
+        MsgBox("a=" & A & "b=" & B & "c=" & C)
+        'MsgBox("a=", A & "b=", B)
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        A = 8 : B = 14 : C = 9
+    End Sub
+End Class
