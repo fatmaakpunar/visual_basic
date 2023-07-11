e4abb9b11336156d7311d98f2d1c06502c3a261a
fatma <123814832+fatmaakpunar@users.noreply.github.com>
2023-07-11 12:32:06 +0300

    Add files via upload

---
+++ a.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'klavyeden girilen matris verilerinin toplamını bulma
+        'değişkenleri tanıt dim m(5,5) as integer
+        'hatırlatma matris olduğu için 2 boyut hatırlanmalı satır ve sütun
+        ' satırı gir
+        'sütunu gir
+        ' m(sat,sut) m(k,j) =veri girişi
+        'text kutusuna yerleştir
+        'matris verilerini topla
+        'sütunun döngüsünü kapat
+        'text kutusuna yerleştir & satır atlat chr(13) & chr(10)
+        'satır döngüsünü kapat
+        'buton üzerine toplamı yazdır
+        Dim m(5, 5), i, j, k, l, top As Integer
+        top = 0
+        TextBox1.Text = ""
+        For k = 1 To 5 'satır
+            For j = 1 To 5 'sütun
+                m(k, j) = InputBox(k & ". satırın " & j & ". sütun verisini giriniz")
+                top = top + m(k, j)
+                TextBox1.Text = TextBox1.Text & m(k, j) & " "
+            Next
+            TextBox1.Text = TextBox1.Text & Chr(13) & Chr(10)
+            Button1.Text = "matris toplamı=" & " " & top
+        Next
+    End Sub
+End Class
---
+++ döngüler.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sayi1, sayi2, toplam, x As Long
+        sayi1 = TextBox1.Text
+        toplam = 0
+        For x = 2 To sayi1 / 2
+            If sayi1 Mod x = 0 Then toplam = toplam + x
+        Next
+        If toplam + 1 = sayi1 Then
+            MsgBox("mükemmel sayı")
+        Else
+            sayi2 = sayi1
+            Do
+                toplam = 0
+                sayi2 = sayi2 + 1
+                For x = 2 To sayi2 / 2
+                    If sayi2 Mod x = 0 Then toplam = toplam + x
+                Next
+            Loop Until toplam + 1 = sayi2
+            MsgBox(sayi2)
+        End If
+    End Sub
+End Class
